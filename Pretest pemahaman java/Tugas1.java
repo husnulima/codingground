953fc90c260bb695de5ea6546e6c0f31eba08151
husnulima <Husnulima959@gmail.com>
2017-02-27 09:17:04 +0700

    Created by Coding Ground Client

---
+++ Pretest pemahaman java/Tugas1.java
@@ -0,0 +1,8 @@
+public class Tugas1{
+
+     public static void main(String []args){
+         String nama = "Husnul";
+         String kota = "Jepara";
+         System.out.println("Nama : " + nama + " Kota : " + kota);
+     }
+}

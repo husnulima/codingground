e7031c26e27e13688de19856516cd47e7d962aa1
husnulima <Husnulima959@gmail.com>
2017-03-05 14:30:40 +0700

    Created by Coding Ground Client

---
+++ Pretest pemahaman java/Tugas8/Biodata8.java
@@ -0,0 +1,9 @@
+public class Biodata8{
+	String nama = "Husnul Khotimah";
+	String alamat = "Jepara";
+	int umur = 21;
+
+	void CetakBiodata(){
+		System.out.println("Nama : " + nama + "\nAlamat : " + alamat + "\nUmur : " + umur);
+	}
+}
